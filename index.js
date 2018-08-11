5af04189e137dadacd2ca9cefaf687471ba22b3c
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-11 15:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const logDriversByHometown = function(drivers, location) {
   })
 }
 
+const driversByRevenue = function(drivers) {
+  return drivers.slice().sort(function(driver1, driver2) {
+    return driver1.revenue - driver2.revenue;
+  })
+}
+
 const driversByName = function(drivers) {
   return drivers.slice().sort(function(driver1, driver2) {
     return driver1.name.localCompare(driver2.name);
