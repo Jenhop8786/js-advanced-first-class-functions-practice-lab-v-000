d5f57e1d0dccbaf26908b5c38c3bf33af5d94958
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-11 15:55:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // Code your solution in this file!
 
 const logDriverNames = function(drivers) {
-  drivers.forEach(function(driver) {
+  drivers.forEach(function(driver){
     console.log(driver.name);
   })
 }
 
 const logDriversByHometown = function(drivers, location) {
-  drivers.forEach(function(driver) {
-    if (driver.hometown === location) {
+  drivers.forEach(function(driver){
+    if (driver.hometown === location){
       console.log(driver.name);
     }
   })
 }
 
 const driversByRevenue = function(drivers) {
-  return drivers.slice().sort(function(driver1, driver2) {
+  return drivers.slice().sort(function (driver1, driver2) {
     return driver1.revenue - driver2.revenue;
   })
 }
 
 const driversByName = function(drivers) {
-  return drivers.slice().sort(function(driver1, driver2) {
-    return driver1.name.localCompare(driver2.name);
+  return drivers.slice().sort(function (driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name);
   })
 }
 
